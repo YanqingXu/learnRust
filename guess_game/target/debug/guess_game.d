11b89fde80d9d31bc876b5d85024073c3292fb73
YanqingXu <xyq949587362xyq007@163.com>
2021-01-10 13:54:04 +0800

    use rand crate

--- guess_game/target/debug/guess_game.d
+++
@@ -1,1 +0,0 @@
-F:\rust\learnRust\guess_game\target\debug\guess_game.exe: F:\rust\learnRust\guess_game\src\main.rs
--- guess_game/target/rls/debug/deps/guess_game-3bba271e3dc7f0de.d
+++
@@ -1,5 +0,0 @@
-f:\rust\learnRust\guess_game\target\rls\debug\deps\guess_game-3bba271e3dc7f0de.rmeta: src\main.rs
-
-f:\rust\learnRust\guess_game\target\rls\debug\deps\guess_game-3bba271e3dc7f0de.d: src\main.rs
-
-src\main.rs:
